0c06372358304f44857c6f9394c7f9190ead404e
E-nani95 <ytew0435@naver.com>
2025-08-28 20:24:38 +0900

    Delete API_Test directory

--- API_Test/src/main/java/com/example/API_Test/SampleAPI/Repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.API_Test.SampleAPI.Repository;
-
-import com.example.API_Test.SampleAPI.Repository.Entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity,Long> {
-}
--- API_Test/src/main/java/com/example/API_Test/SampleAPI/Service/TestService.java
+++
@@ -1,7 +0,0 @@
-package com.example.API_Test.SampleAPI.Service;
-
-import com.example.API_Test.SampleAPI.Controller.DTO.Request_Detection_Front;
-
-public interface TestService {
-    String API_Front_Test(String both, String path);
-}
